7a1b5b2d4be2b34529a1b115e2dc1d01e394f2a9
Mate Peter <peter.mate@balabit.com>
2016-01-12 16:48:54 +0100

    Architecture plan

--- src/main/java/org/jenkinsci/plugins/workspacelayering/WorkspaceLayering.java
+++ src/main/java/org/jenkinsci/plugins/workspacelayering/WorkspaceLayering.java
@@ -1,7 +1,19 @@
 package org.jenkinsci.plugins.workspacelayering;
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.Proc;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
 import hudson.model.Descriptor;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.remoting.Channel;
+import hudson.remoting.VirtualChannel;
 import hudson.tasks.BuildWrapper;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class WorkspaceLayering extends BuildWrapper {
@@ -13,13 +25,23 @@ public class WorkspaceLayering extends BuildWrapper {
         this.workspaceLayering = workspaceLayering;
     }
 
-    /**
-     * We'll use this from the <tt>config.jelly</tt>.
-     */
     public boolean getWorkspaceLayering() {
         return workspaceLayering;
     }
     
+    @Override
+    public Launcher decorateLauncher(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException, Run.RunnerAbortedException {
+        //do layering
+        return launcher;
+    }
+    
+    @Override
+     public Environment setUp(AbstractBuild build, Launcher launcher,
+            BuildListener listener) throws IOException, InterruptedException {
+            return new LayeredEnvironment();
+     }
+     
+     
     @Extension
     public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
 
@@ -27,10 +49,18 @@ public class WorkspaceLayering extends BuildWrapper {
              * This human readable name is used in the configuration screen.
              */
             public String getDisplayName() {
-                    // TODO localization
                     return "Use workspace layering";
             }
 
+    }
+
+    private class LayeredEnvironment extends Environment {
+
+        @Override
+        public boolean tearDown(AbstractBuild build, BuildListener listener) throws IOException, InterruptedException {
+            //remove layer
+            return super.tearDown(build, listener);
+        }
     }
 }
 
